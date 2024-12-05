4f78366cca5f1fb111af082b6076d74938d580ae
Nikola Icic <nikolaicic@gmail.com>
2024-12-05 17:06:09 +0100

    ?

--- 2024/Advent2024/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ 2024/Advent2024/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1883aa7ff8f437be7e7a28aa1d5821d582cfc759")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+827cacce7fbda95a85da8de35bdc8b67cc2bf8b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
