1db9d60eb24c9e623d062e3ad8d34de62cbb425a
Nikola Icic <nikolaicic@gmail.com>
2024-12-07 14:43:32 +0100

    Day 5

---
+++ 2024/Advent2024/Day5.cs
@@ -0,0 +1,114 @@
+﻿namespace Advent2024
+{
+    public class Day5
+    {
+        public List<Rule> Rules { get; set; } = new List<Rule>();
+        public List<Update> Updates { get; set; } = new List<Update>();
+
+        public void ReadInput(string input)
+        {
+            input = input.Replace("\n", "");
+            string[] rows = input.Split('\r');
+
+            int index = 0;
+            for (int i = 0; i < rows.Length; i++)
+            {
+                if (rows[i] == "")
+                {
+                    index = i;
+                    break;
+                }
+                string[] nums = rows[i].Split('|');
+                Rules.Add(new Rule()
+                {
+                    Num1 = int.Parse(nums[0]),
+                    Num2 = int.Parse(nums[1])
+                });
+            }
+            for (int i = index + 1; i < rows.Length; i++)
+            {
+                string[] nums = rows[i].Split(',');
+                Update u = new Update();
+                foreach (string n in nums)
+                    u.Pages.Add(int.Parse(n));
+                Updates.Add(u);
+            }
+        }
+
+        public int SumCorrectUpdateMiddle(string input)
+        {
+            int sum = 0;
+
+            ReadInput(input);
+            foreach (Update u in Updates)
+                if (u.IsInOrder(Rules))
+                    sum += u.FindMiddleNumber();
+
+            return sum;
+        }
+        public int SumIncorrectUpdateMiddle(string input)
+        {
+            ReadInput(input);
+
+            int sum = 0;
+            Parallel.ForEach(Updates, u =>
+            {
+                if (!u.IsInOrder(Rules))
+                {
+                    u.FixUpdate(Rules);
+                    Interlocked.Add(ref sum, u.FindMiddleNumber());
+                }
+            });
+
+            return sum;
+        }
+    }
+
+    public class Rule
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+    }
+
+    public class Update
+    {
+        public List<int> Pages { get; set; } = new List<int>();
+
+        public int FindMiddleNumber()
+        {
+            return Pages[Pages.Count / 2];
+        }
+
+        public bool IsInOrder(List<Rule> rules)
+        {
+            foreach (Rule rule in rules)
+            {
+                int i1 = Pages.IndexOf(rule.Num1);
+                if (i1 == -1) continue;
+                int i2 = Pages.IndexOf(rule.Num2);
+                if (i2 == -1) continue;
+                if (i1 > i2) return false;
+            }
+            return true;
+        }
+
+        public void FixUpdate(List<Rule> rules)
+        {
+            while (!IsInOrder(rules))
+            {
+                foreach (Rule rule in rules)
+                {
+                    int i1 = Pages.IndexOf(rule.Num1);
+                    int i2 = Pages.IndexOf(rule.Num2);
+                    if (i1 == -1 || i2 == -1 || i1 < i2) continue;
+                    Swap(Pages, i1, i2);
+                }
+            }
+        }
+
+        private static void Swap(List<int> numbers, int i, int j)
+        {
+            (numbers[j], numbers[i]) = (numbers[i], numbers[j]);
+        }
+    }
+}
---
+++ 2024/Tests/Day5Tests.cs
@@ -0,0 +1,109 @@
+﻿using Advent2024;
+
+namespace Tests
+{
+    public class Day5Tests
+    {
+        Day5 d5;
+        public Day5Tests()
+        {
+            d5 = new Day5();
+        }
+
+        [Fact]
+        public void CanReadInput_Rules()
+        {
+            string input = "" +
+                "75|14\r\n" +
+                "53|13\r\n" +
+                "\r\n" +
+                "75,47,61,53,29\r\n" +
+                "97,61,53,29,13";
+
+            d5.ReadInput(input);
+
+            Assert.Equal(2, d5.Rules.Count);
+            Assert.Equal(75, d5.Rules[0].Num1);
+            Assert.Equal(13, d5.Rules[1].Num2);
+        }
+        [Fact]
+        public void CanReadInput_Updates()
+        {
+            string input = "" +
+                "75|14\r\n" +
+                "\r\n" +
+                "75,47,61,53,29\r\n" +
+                "97,61,53,29,13";
+
+            d5.ReadInput(input);
+
+            Assert.Equal(2, d5.Updates.Count);
+            Assert.Equal(5, d5.Updates[0].Pages.Count);
+            Assert.Equal(97, d5.Updates[1].Pages[0]);
+            Assert.Equal(13, d5.Updates[1].Pages[4]);
+        }
+        [Fact]
+        public void IsInOrder_ShouldReturnCorrect()
+        {
+            Update u = new Update();
+            u.Pages = new List<int> { 1, 2, 3, 4, 5, 6 };
+            List<Rule> rules = new List<Rule>() {
+                new Rule() { Num1 = 99, Num2 = 102},
+                new Rule{ Num1 = 4, Num2 = 6 }
+            };
+            List<Rule> rules2 = new List<Rule>() {
+                new Rule() { Num1 = 1, Num2 = 2},
+                new Rule{ Num1 = 5, Num2 = 2 }
+            };
+
+            Assert.True(u.IsInOrder(rules));
+            Assert.False(u.IsInOrder(rules2));
+
+        }
+        [Fact]
+        public void FindMiddleNumber_ShouldReturnMiddleNumber()
+        {
+            Update u = new Update() { Pages = { 1, 2, 3, 7, 4, 5, 6 } };
+            Update u2 = new Update() { Pages = { 1, 2, 3 } };
+
+            Assert.Equal(7, u.FindMiddleNumber());
+            Assert.Equal(2, u2.FindMiddleNumber());
+        }
+        [Fact]
+        public void SumCorrectUpdateMiddle_AcceptanceTest()
+        {
+            string input = "47|53\r\n97|13\r\n97|61\r\n97|47\r\n75|29" +
+                "\r\n61|13\r\n75|53\r\n29|13\r\n97|29\r\n53|29\r\n61|53" +
+                "\r\n97|53\r\n61|29\r\n47|13\r\n75|47\r\n97|75\r\n47|61" +
+                "\r\n75|61\r\n47|29\r\n75|13\r\n53|13\r\n\r\n75,47,61,53,29" +
+                "\r\n97,61,53,29,13\r\n75,29,13\r\n75,97,47,61,53\r\n61,13,29\r\n97,13,75,29,47";
+
+            Assert.Equal(143, d5.SumCorrectUpdateMiddle(input));
+        }
+        [Fact]
+        public void Part1()
+        {
+            string text = File.ReadAllText(@"D:\Projects\AoC\2024\Tests\Input5.txt");
+
+            Assert.Equal(4905, d5.SumCorrectUpdateMiddle(text));
+        }
+        [Fact]
+        public void SumIncorrectUpdateMiddle_AcceptanceTest()
+        {
+            string input = "47|53\r\n97|13\r\n97|61\r\n97|47\r\n75|29" +
+                "\r\n61|13\r\n75|53\r\n29|13\r\n97|29\r\n53|29\r\n61|53" +
+                "\r\n97|53\r\n61|29\r\n47|13\r\n75|47\r\n97|75\r\n47|61" +
+                "\r\n75|61\r\n47|29\r\n75|13\r\n53|13\r\n\r\n75,47,61,53,29" +
+                "\r\n97,61,53,29,13\r\n75,29,13\r\n75,97,47,61,53\r\n61,13,29\r\n97,13,75,29,47";
+
+            Assert.Equal(123, d5.SumIncorrectUpdateMiddle(input));
+        }
+        [Fact]
+        public void Part2()
+        {
+            string text = File.ReadAllText(@"D:\Projects\AoC\2024\Tests\Input5.txt");
+
+            Assert.Equal(6204, d5.SumIncorrectUpdateMiddle(text));
+        }
+    }
+}
