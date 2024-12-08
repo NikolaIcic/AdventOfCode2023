9b255fcec40417d49f3df7c842d0f3ade6a5052b
Nikola Icic <nikolaicic@gmail.com>
2024-12-08 22:50:53 +0100

    Day7

---
+++ 2024/Advent2024/Day7.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Advent2024
+{
+    public class Day7
+    {
+        public List<Equasion> Equasions { get; set; } = new List<Equasion>();
+        public int MaxLength = 0;
+
+        public void ReadInput(string input)
+        {
+            input = input.Replace("\n", "");
+            string[] rows = input.Split('\r');
+            foreach (string row in rows)
+            {
+                string[] parts = row.Split(": ");
+                Equasion e = new Equasion();
+                e.Result = double.Parse(parts[0]);
+                string[] nums = parts[1].Split(" ");
+                foreach (string num in nums)
+                    e.Numbers.Add(int.Parse(num));
+                if (nums.Length > MaxLength)
+                    MaxLength = nums.Length;
+                Equasions.Add(e);
+            }
+        }
+
+        public double TotalCalibration(string input)
+        {
+            ReadInput(input);
+            double sum = 0;
+            var opps = PermutationGenerator.GeneratePermutations(MaxLength, new List<char> { '*', '+' });
+            foreach (Equasion e in Equasions)
+                if (e.IsPossible(opps))
+                    sum += e.Result;
+            return sum;
+        }
+
+        public double TotalCalibrationExtended(string input)
+        {
+            ReadInput(input);
+            ulong sum = 0;
+            var opps = PermutationGenerator.GeneratePermutations(MaxLength, new List<char> { '*', '+', '|' });
+
+            Parallel.ForEach(Equasions, e =>
+            {
+                if (e.IsPossible(opps))
+                    Interlocked.Add(ref sum, (ulong)e.Result);
+            });
+            return sum;
+        }
+    }
+
+    public class Equasion
+    {
+        public double Result { get; set; }
+        public List<int> Numbers { get; set; } = new List<int>();
+
+        public bool IsPossible(List<List<char>> operations)
+        {
+            for (int j = 0; j < operations.Count; j++)
+            {
+                if (operations[j].Count < Numbers.Count - 1)
+                    continue;
+                if (operations[j].Count > Numbers.Count - 1)
+                    break;
+
+                double current = Numbers[0];
+                for (int i = 1; i < Numbers.Count; i++)
+                {
+                    if (operations[j][i - 1] == '+')
+                        current += Numbers[i];
+                    else if (operations[j][i - 1] == '*')
+                        current *= Numbers[i];
+                    else
+                        current = double.Parse(current.ToString() + Numbers[i].ToString());
+                }
+                if (current == Result)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+    public static class PermutationGenerator
+    {
+        public static List<List<char>> GeneratePermutations(int maxLength, List<char> toPermutate)
+        {
+            List<List<char>> result = new List<List<char>>();
+
+            for (int length = 1; length <= maxLength; length++)
+            {
+                GeneratePermutationsHelper(result, new List<char>(), length, toPermutate);
+            }
+
+            return result;
+        }
+
+        private static void GeneratePermutationsHelper(List<List<char>> result, List<char> current, int length, List<char> toPermutate)
+        {
+            if (current.Count == length)
+            {
+                result.Add(new List<char>(current));
+                return;
+            }
+
+            foreach (char c in toPermutate)
+            {
+                current.Add(c);
+                GeneratePermutationsHelper(result, current, length, toPermutate);
+                current.RemoveAt(current.Count - 1);
+            }
+        }
+    }
+}
---
+++ 2024/Tests/Day7Tests.cs
@@ -0,0 +1,95 @@
+﻿using Advent2024;
+
+namespace Tests
+{
+    public class Day7Tests
+    {
+        Day7 d7 = new Day7();
+        Equasion eq = new Equasion();
+
+        [Fact]
+        public void Test()
+        {
+            Day6 e = new Day6();
+        }
+        [Fact]
+        public void CanReadInput()
+        {
+            string input = "190: 10 19\r\n3267: 81 40 27";
+
+            d7.ReadInput(input);
+
+            Assert.Equal(190, d7.Equasions[0].Result);
+            Assert.Equal(3267, d7.Equasions[1].Result);
+            Assert.Equal(10, d7.Equasions[0].Numbers[0]);
+            Assert.Equal(27, d7.Equasions[1].Numbers[2]);
+            Assert.Equal(3, d7.MaxLength);
+        }
+        [Fact]
+        public void IsEquasionPossible()
+        {
+            Equasion e = new Equasion()
+            {
+                Result = 190,
+                Numbers = new List<int> { 10, 19 }
+            };
+            Equasion e2 = new Equasion()
+            {
+                Result = 3267,
+                Numbers = new List<int> { 81,40,27 }
+            };
+            Equasion e3 = new Equasion()
+            {
+                Result = 7290,
+                Numbers = new List<int> { 6,8,6,15 }
+            };
+            Equasion e4 = new Equasion()
+            {
+                Result = 292,
+                Numbers = new List<int> { 11,6,16,20 }
+            };
+
+            var operations = PermutationGenerator.GeneratePermutations(3, new List<char> {'+','*'});
+
+            Assert.True(e.IsPossible(operations));
+            Assert.True(e2.IsPossible(operations));
+            Assert.False(e3.IsPossible(operations));
+            Assert.True(e4.IsPossible(operations));
+        }
+        [Fact]
+        public void TestPermutationGenerator()
+        {
+            var res = PermutationGenerator.GeneratePermutations(10, new List<char> { '+', '*' });
+
+            Assert.Equal(2046, res.Count);
+        }
+        [Fact]
+        public void TotalCalibration_AcceptanceTest()
+        {
+            string input = "190: 10 19\r\n3267: 81 40 27\r\n83: 17 5\r\n156: 15 6\r\n7290: 6 8 6 15\r\n161011: 16 10 13\r\n192: 17 8 14\r\n21037: 9 7 18 13\r\n292: 11 6 16 20";
+
+            Assert.Equal(3749, d7.TotalCalibration(input));
+        }
+        [Fact]
+        public void Part1()
+        {
+            string text = File.ReadAllText(@"D:\Projects\AoC\2024\Tests\Input7.txt");
+
+            Assert.Equal(1582598718861, d7.TotalCalibration(text));
+        }
+        [Fact]
+        public void TotalCalibrationExtended_AcceptanceTest()
+        {
+            string input = "190: 10 19\r\n3267: 81 40 27\r\n83: 17 5\r\n156: 15 6\r\n7290: 6 8 6 15\r\n161011: 16 10 13\r\n192: 17 8 14\r\n21037: 9 7 18 13\r\n292: 11 6 16 20";
+
+            Assert.Equal(11387, d7.TotalCalibrationExtended(input));
+        }
+        [Fact(Skip = "Medium excecution time + Paralellisation")]
+        public void Part2()
+        {
+            string text = File.ReadAllText(@"D:\Projects\AoC\2024\Tests\Input7.txt");
+
+            Assert.Equal(165278151522644, d7.TotalCalibrationExtended(text));
+        }
+    }
+}
