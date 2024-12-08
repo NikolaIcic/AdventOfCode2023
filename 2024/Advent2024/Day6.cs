fe34a9e7f616ec4d26d621acb394d6a19d68107a
Nikola Icic <nikolaicic@gmail.com>
2024-12-08 21:11:22 +0100

    Day 6

---
+++ 2024/Advent2024/Day6.cs
@@ -0,0 +1,190 @@
+﻿namespace Advent2024
+{
+    public class Day6
+    {
+        public int GetGuardMoves(string input)
+        {
+            Map m = new Map();
+            m.ReadInput(input);
+            return m.GetGuardMoves();
+        }
+
+        public int GetAllInfinitePath(string input)
+        {
+            Map m = new Map();
+            m.ReadInput(input);
+            return m.GetAllInfinitePaths();
+        }
+    }
+    public class Map
+    {
+        public int Rows { get; set; }
+        public int Cols { get; set; }
+        public char[,] Matrix { get; set; }
+
+        public void ReadInput(string input)
+        {
+            input = input.Replace("\n", "");
+            string[] rows = input.Split('\r');
+            Rows = rows.Length;
+            Cols = rows[0].Length;
+            Matrix = new char[Rows, Cols];
+            for (int i = 0; i < rows.Length; i++)
+                for (int j = 0; j < rows[i].Length; j++)
+                    Matrix[i, j] = rows[i][j];
+        }
+
+        public int GetGuardMoves()
+        {
+            int count = 0;
+            char dir = '^';
+            int guardX = 0;
+            int guardY = 0;
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Cols; j++)
+                    if (Matrix[i, j] == '^' || Matrix[i, j] == '>' || Matrix[i, j] == '<' || Matrix[i, j] == 'v')
+                    {
+                        dir = Matrix[i, j];
+                        guardX = j;
+                        guardY = i;
+                    }
+            char[,] traversed = new char[Rows, Cols];
+            while (true)
+            {
+                if (traversed[guardX, guardY] != 'X')
+                {
+                    count++;
+                    traversed[guardX, guardY] = 'X';
+                }
+
+                int nextX = guardX;
+                int nextY = guardY;
+                switch (dir)
+                {
+                    case '^':
+                        nextY--;
+                        break;
+                    case '>':
+                        nextX++;
+                        break;
+                    case '<':
+                        nextX--;
+                        break;
+                    case 'v':
+                        nextY++;
+                        break;
+                }
+                if (nextX < 0 || nextX >= Cols || nextY < 0 || nextY >= Rows)
+                    break;
+                if (Matrix[nextY, nextX] == '#')
+                    dir = Rotate90Deg(dir);
+                else
+                {
+                    guardX = nextX;
+                    guardY = nextY;
+                }
+            }
+
+            return count;
+        }
+
+        public bool IsPathInfinite()
+        {
+            char dir = '^';
+            int guardX = 0;
+            int guardY = 0;
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Cols; j++)
+                    if (Matrix[i, j] != '.' && Matrix[i, j] != '#')
+                    {
+                        dir = Matrix[i, j];
+                        guardX = j;
+                        guardY = i;
+                    }
+            char[,,] obstaclesTraversed = new char[Rows, Cols, 4];
+            while (true)
+            {
+                int nextX = guardX;
+                int nextY = guardY;
+                switch (dir)
+                {
+                    case '^':
+                        nextY--;
+                        break;
+                    case '>':
+                        nextX++;
+                        break;
+                    case '<':
+                        nextX--;
+                        break;
+                    case 'v':
+                        nextY++;
+                        break;
+                }
+                if (nextX < 0 || nextX >= Cols || nextY < 0 || nextY >= Rows)
+                    break;
+                if (Matrix[nextY, nextX] == '#')
+                {
+                    if (obstaclesTraversed[nextY, nextX, DirToInt(dir)] == '#')
+                        return true;
+                    obstaclesTraversed[nextY, nextX, DirToInt(dir)] = '#';
+                    dir = Rotate90Deg(dir);
+                }
+                else
+                {
+                    guardX = nextX;
+                    guardY = nextY;
+                }
+            }
+            return false;
+        }
+
+        public int GetAllInfinitePaths()
+        {
+            int count = 0;
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Cols; j++)
+                {
+                    char temp = Matrix[i, j];
+                    Matrix[i, j] = '#';
+                    if (IsPathInfinite())
+                        count++;
+                    Matrix[i, j] = temp;
+                }
+            return count;
+        }
+
+        private static char Rotate90Deg(char dir)
+        {
+            switch (dir)
+            {
+                case '^':
+                    return '>';
+                case '>':
+                    return 'v';
+                case 'v':
+                    return '<';
+                case '<':
+                    return '^';
+                default:
+                    return '^';
+            }
+        }
+
+        private static int DirToInt(char dir)
+        {
+            switch (dir)
+            {
+                case '^':
+                    return 0;
+                case '>':
+                    return 1;
+                case '<':
+                    return 2;
+                case 'v':
+                    return 3;
+                default: return 0;
+            }
+        }
+    }
+}
---
+++ 2024/Tests/Day6Tests.cs
@@ -0,0 +1,98 @@
+﻿using Advent2024;
+
+namespace Tests
+{
+    public class Day6Tests
+    {
+        Day6 d6 = new Day6();
+        Map m = new Map();
+        [Fact]
+        public void Test()
+        {
+            Day6 e = new Day6();
+            Map e2 = new Map();
+        }
+        [Fact]
+        public void CanReadMap()
+        {
+            string input = "...\r\n";
+            input += "..#";
+            m.ReadInput(input);
+
+            Assert.Equal(2, m.Rows);
+            Assert.Equal(3, m.Cols);
+            Assert.Equal('.', m.Matrix[0, 0]);
+            Assert.Equal('#', m.Matrix[1, 2]);
+        }
+        [Fact]
+        public void GetGuardMoves_CanGetSimple()
+        {
+            string input = "";
+            input += "...\r";
+            input += ".^.";
+
+            string input2 = "";
+            input2 += "...\r";
+            input2 += ">..\r";
+            input2 += "...";
+
+            m.ReadInput(input);
+            Assert.Equal(2, m.GetGuardMoves());
+            m.ReadInput(input2);
+            Assert.Equal(3, m.GetGuardMoves());
+        }
+        [Fact]
+        public void GetGuardMoves_WithObstacles()
+        {
+            string input2 = "";
+            input2 += "....\r";
+            input2 += ">.#.\r";
+            input2 += "#...\r";
+            input2 += ".#..";
+
+            m.ReadInput(input2);
+            Assert.Equal(4, m.GetGuardMoves());
+        }
+        [Fact]
+        public void GetGuardMoves_AcceptanceTest()
+        {
+            string input = "....#.....\r\n.........#\r\n..........\r\n..#.......\r\n.......#..\r\n..........\r\n.#..^.....\r\n........#.\r\n#.........\r\n......#...";
+
+            Assert.Equal(41,d6.GetGuardMoves(input));
+        }
+        [Fact]
+        public void Part1()
+        {
+            string text = File.ReadAllText(@"D:\Projects\AoC\2024\Tests\Input6.txt");
+
+            Assert.Equal(5177, d6.GetGuardMoves(text));
+        }
+        [Fact]
+        public void IsPathInfinite_Test()
+        {
+            string input2 = "";
+            input2 += "##...\r";
+            input2 += "^...#\r";
+            input2 += "#....\r";
+            input2 += "...#.";
+
+            m.ReadInput(input2);
+
+            Assert.True(m.IsPathInfinite());
+        }
+        [Fact]
+        public void GetAllInfinitePath_AcceptanceTest()
+        {
+            string input = "....#.....\r\n.........#\r\n..........\r\n..#.......\r\n.......#..\r\n..........\r\n.#..^.....\r\n........#.\r\n#.........\r\n......#...";
+            
+            Assert.Equal(6, d6.GetAllInfinitePath(input));
+        }
+        [Fact]
+        public void Part2()
+        {
+            string text = File.ReadAllText(@"D:\Projects\AoC\2024\Tests\Input6.txt");
+
+            Assert.Equal(1686, d6.GetAllInfinitePath(text));
+        }
+    }
+}
