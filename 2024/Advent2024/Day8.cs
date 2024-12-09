e10a474d2fb6972f8e7316b85f3ef5147ef9db9a
Nikola Icic <nikolaicic@gmail.com>
2024-12-09 16:39:21 +0100

    Day 8

---
+++ 2024/Advent2024/Day8.cs
@@ -0,0 +1,141 @@
+﻿namespace Advent2024
+{
+    public class Day8
+    {
+        public List<Antenna> Antennas { get; set; } = new List<Antenna>();
+        public int Rows { get; set; }
+        public int Cols { get; set; }
+
+        public void ReadInput(string input)
+        {
+            input = input.Replace("\n", "");
+            string[] rows = input.Split('\r');
+            for (int i = 0; i < rows.Length; i++)
+                for (int j = 0; j < rows[i].Length; j++)
+                    if (rows[i][j] != '.')
+                        Antennas.Add(new Antenna()
+                        {
+                            Frequency = rows[i][j],
+                            X = j,
+                            Y = i
+                        });
+            Rows = rows.Length;
+            Cols = rows[0].Length;
+        }
+        public List<AntiNode> GetAntiNodes(Antenna a1, Antenna a2)
+        {
+            if (a1.Frequency != a2.Frequency) return null;
+            List<AntiNode> res = new List<AntiNode>();
+
+            int v1X = a2.X - a1.X;
+            int v1Y = a2.Y - a1.Y;
+
+            int v2X = a1.X - a2.X;
+            int v2Y = a1.Y - a2.Y;
+
+            res.Add(new AntiNode()
+            {
+                X = a1.X + v2X,
+                Y = a1.Y + v2Y
+            });
+            res.Add(new AntiNode()
+            {
+                X = a2.X + v1X,
+                Y = a2.Y + v1Y
+            });
+
+            return res;
+        }
+        public List<AntiNode> GetAntiNodesResonant(Antenna a1, Antenna a2)
+        {
+            if (a1.Frequency != a2.Frequency) return null;
+            List<AntiNode> res = new List<AntiNode>();
+
+            int v1X = a2.X - a1.X;
+            int v1Y = a2.Y - a1.Y;
+
+            int v2X = a1.X - a2.X;
+            int v2Y = a1.Y - a2.Y;
+
+            int x = 0;
+            int y = 0;
+            int count = 0;
+            while (x >= 0 && x < Rows && y >= 0 && y < Cols)
+            {
+                x = a1.X + v2X * count;
+                y = a1.Y + v2Y * count;
+                res.Add(new AntiNode()
+                {
+                    X = x,
+                    Y = y
+                });
+                count++;
+            }
+            x = 0;
+            y = 0;
+            count = 0;
+            while (x >= 0 && x < Rows && y >= 0 && y < Cols)
+            {
+                x = a2.X + v1X * count;
+                y = a2.Y + v1Y * count;
+                res.Add(new AntiNode()
+                {
+                    X = x,
+                    Y = y
+                });
+                count++;
+            }
+
+            return res;
+        }
+        public int GetUniqueAntiNodes(string input)
+        {
+            ReadInput(input);
+            int count = 0;
+            List<AntiNode> antiNodes = new List<AntiNode>();
+            for (int i = 0; i < Antennas.Count - 1; i++)
+                for (int j = i + 1; j < Antennas.Count; j++)
+                    if (Antennas[i].Frequency == Antennas[j].Frequency)
+                        foreach (AntiNode anti in GetAntiNodes(Antennas[i], Antennas[j]))
+                            if (anti.X >= 0 && anti.X < Cols && anti.Y >= 0 && anti.Y < Rows &&
+                                antiNodes.Find(a => a.X == anti.X && a.Y == anti.Y) == null)
+                            {
+                                count++;
+                                antiNodes.Add(anti);
+                            }
+
+            return count;
+        }
+        public int GetUniqueAntiNodesResonant(string input)
+        {
+            ReadInput(input);
+            int count = 0;
+            List<AntiNode> antiNodes = new List<AntiNode>();
+            for (int i = 0; i < Antennas.Count - 1; i++)
+                for (int j = i + 1; j < Antennas.Count; j++)
+                    if (Antennas[i].Frequency == Antennas[j].Frequency)
+                        foreach (AntiNode anti in GetAntiNodesResonant(Antennas[i], Antennas[j]))
+                            if (anti.X >= 0 && anti.X < Cols && anti.Y >= 0 && anti.Y < Rows &&
+                                antiNodes.Find(a => a.X == anti.X && a.Y == anti.Y) == null)
+                            {
+                                count++;
+                                antiNodes.Add(anti);
+                            }
+
+            return count;
+        }
+    }
+
+    public class Antenna
+    {
+        public char Frequency { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class AntiNode
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ 2024/Tests/Day8Tests.cs
@@ -0,0 +1,81 @@
+﻿using Advent2024;
+
+namespace Tests
+{
+    public class Day8Tests
+    {
+        Day8 d8;
+
+        public Day8Tests()
+        {
+            d8 = new Day8();
+        }
+
+        [Fact]
+        public void CanReadInput()
+        {
+            string input = "......\r\n..ab..";
+
+            d8.ReadInput(input);
+
+            Assert.Equal(2, d8.Antennas.Count);
+            Assert.Equal('a', d8.Antennas[0].Frequency);
+            Assert.Equal(2, d8.Antennas[0].X);
+            Assert.Equal(1, d8.Antennas[0].Y);
+            Assert.Equal(2, d8.Rows);
+            Assert.Equal(6, d8.Cols);
+        }
+
+        [Fact]
+        public void GetAntiNodes_ShouldReturnCorrect()
+        {
+            Antenna a1 = new Antenna() { Frequency = 'A', X = 13, Y = 5 };
+            Antenna a2 = new Antenna() { Frequency = 'A', X = 10, Y = 6 };
+
+            Antenna a3 = new Antenna() { Frequency = 'A', X = 10, Y = 9 };
+            Antenna a4 = new Antenna() { Frequency = 'A', X = 5, Y = 8 };
+
+            var res = d8.GetAntiNodes(a1, a2);
+            var res2 = d8.GetAntiNodes(a3, a4);
+
+            Assert.Equal(16, res[0].X);
+            Assert.Equal(4, res[0].Y);
+            Assert.Equal(7, res[1].X);
+            Assert.Equal(7, res[1].Y);
+
+            Assert.Equal(15, res2[0].X);
+            Assert.Equal(10, res2[0].Y);
+            Assert.Equal(0, res2[1].X);
+            Assert.Equal(7, res2[1].Y);
+        }
+        [Fact]
+        public void GetUniqueAntiNodes_AcceptanceTest()
+        {
+            string input = "............\r\n........0...\r\n.....0......\r\n.......0....\r\n....0.......\r\n......A.....\r\n............\r\n............\r\n........A...\r\n.........A..\r\n............\r\n............";
+
+            Assert.Equal(14, d8.GetUniqueAntiNodes(input));
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            string text = File.ReadAllText(@"D:\Projects\AoC\2024\Tests\Input8.txt");
+
+            Assert.Equal(369, d8.GetUniqueAntiNodes(text));
+        }
+        [Fact]
+        public void GetResonantAntiNodes_AcceptanceTest()
+        {
+            string input = "............\r\n........0...\r\n.....0......\r\n.......0....\r\n....0.......\r\n......A.....\r\n............\r\n............\r\n........A...\r\n.........A..\r\n............\r\n............";
+
+            Assert.Equal(34, d8.GetUniqueAntiNodesResonant(input));
+        }
+        [Fact]
+        public void Part2()
+        {
+            string text = File.ReadAllText(@"D:\Projects\AoC\2024\Tests\Input8.txt");
+
+            Assert.Equal(1169, d8.GetUniqueAntiNodesResonant(text));
+        }
+    }
+}
