9bef1a3e74b74af703f270e227c6b747e0cc517b
Nikola Icic <nikolaicic@gmail.com>
2024-12-16 21:37:59 +0100

    Day 10

---
+++ 2024/Advent2024/Day10.cs
@@ -0,0 +1,112 @@
+﻿namespace Advent2024
+{
+    public class Day10
+    {
+        public int[,] Map;
+        public int Rows;
+        public int Cols;
+
+        public void ReadInput(string input)
+        {
+            input = input.Replace("\n", "");
+            string[] rows = input.Split('\r');
+            Rows = rows.Length;
+            Cols = rows[0].Length;
+            Map = new int[rows.Length, rows[0].Length];
+            for (int i = 0; i < rows.Length; i++)
+                for (int j = 0; j < rows[i].Length; j++)
+                    Map[i, j] = int.Parse(rows[i][j].ToString());
+        }
+
+        public int FindTrailheadSum(string input)
+        {
+            ReadInput(input);
+            int sum = 0;
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Cols; j++)
+                    if (Map[i, j] == 0)
+                        sum += FindTrailheadScore(j, i);
+            return sum;
+        }
+
+        public int FindTrailheadSumDistinct(string input)
+        {
+            ReadInput(input);
+            int sum = 0;
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Cols; j++)
+                    if (Map[i, j] == 0)
+                        sum += FindTrailheadScoreDistinct(j, i);
+            return sum;
+        }
+
+        public int FindTrailheadScore(int x, int y)
+        {
+            int count = 0;
+
+            List<PathNode> traversed = new List<PathNode>();
+
+            count += IsTrail(x + 1, y, x, y, 0, traversed);
+            count += IsTrail(x - 1, y, x, y, 0, traversed);
+            count += IsTrail(x, y + 1, x, y, 0, traversed);
+            count += IsTrail(x, y - 1, x, y, 0, traversed);
+
+            return count;
+        }
+
+        public int FindTrailheadScoreDistinct(int x, int y)
+        {
+            int count = 0;
+
+            count += IsDistrinctTrail(x + 1, y, x, y, 0);
+            count += IsDistrinctTrail(x - 1, y, x, y, 0);
+            count += IsDistrinctTrail(x, y + 1, x, y, 0);
+            count += IsDistrinctTrail(x, y - 1, x, y, 0);
+
+            return count;
+        }
+
+        private int IsTrail(int x, int y, int prevX, int prevY, int score, List<PathNode> traversed)
+        {
+            if (x < 0 || x >= Cols || y < 0 || y >= Rows)
+                return 0;
+            if (Map[y, x] != (Map[prevY, prevX] + 1))
+                return 0;
+            if (Map[y, x] == 9)
+            {
+                if (traversed.Find(n => n.X == x && n.Y == y) != null)
+                    return 0;
+                traversed.Add(new PathNode() { X = x, Y = y });
+                return 1;
+            }
+
+            score += IsTrail(x + 1, y, x, y, 0, traversed);
+            score += IsTrail(x - 1, y, x, y, 0, traversed);
+            score += IsTrail(x, y + 1, x, y, 0, traversed);
+            score += IsTrail(x, y - 1, x, y, 0, traversed);
+            return score;
+        }
+
+        private int IsDistrinctTrail(int x, int y, int prevX, int prevY, int score)
+        {
+            if (x < 0 || x >= Cols || y < 0 || y >= Rows)
+                return 0;
+            if (Map[y, x] != (Map[prevY, prevX] + 1))
+                return 0;
+            if (Map[y, x] == 9)
+                return 1;
+
+            score += IsDistrinctTrail(x + 1, y, x, y, 0);
+            score += IsDistrinctTrail(x - 1, y, x, y, 0);
+            score += IsDistrinctTrail(x, y + 1, x, y, 0);
+            score += IsDistrinctTrail(x, y - 1, x, y, 0);
+            return score;
+        }
+    }
+
+    class PathNode
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ 2024/Tests/Day10Tests.cs
@@ -0,0 +1,87 @@
+﻿using Advent2024;
+
+namespace Tests
+{
+    public class Day10Tests
+    {
+        Day10 d10 = new Day10();
+
+        [Fact]
+        public void CanReadInput()
+        {
+            d10.ReadInput("0123\r\n1234\r\n8765\r\n9876");
+
+            Assert.Equal(0, d10.Map[0, 0]);
+            Assert.Equal(6, d10.Map[3, 3]);
+            Assert.Equal(4, d10.Rows);
+            Assert.Equal(4, d10.Cols);
+        }
+
+        [Fact]
+        public void FindTrailheadScore()
+        {
+            string input = "";
+            input += "0123\r";
+            input += "1234\r";
+            input += "8765\r";
+            input += "9876";
+
+            string input2 = "";
+            input2 += "0123\r";
+            input2 += "2134\r";
+            input2 += "1165\r";
+            input2 += "9876";
+
+            d10.ReadInput(input);
+            Assert.Equal(16, d10.FindTrailheadScoreDistinct(0, 0));
+            d10.ReadInput(input2);
+            Assert.Equal(4, d10.FindTrailheadScoreDistinct(0, 0));
+        }
+        [Fact]
+        public void FindTHSum()
+        {
+            string input = "";
+            input += "0123\r";
+            input += "1234\r";
+            input += "8765\r";
+            input += "9876";
+
+            string input2 = "";
+            input2 += "0123\r";
+            input2 += "2134\r";
+            input2 += "1165\r";
+            input2 += "9876";
+
+            string input3 = "";
+            input3 += "0123456789\r";
+            input3 += "1114111111\r";
+            input3 += "2345611111\r";
+            input3 += "3456789111";
+
+            Assert.Equal(16, d10.FindTrailheadSumDistinct(input));
+            Assert.Equal(4, d10.FindTrailheadSumDistinct(input2));
+            Assert.Equal(8, d10.FindTrailheadSumDistinct(input3));
+        }
+        [Fact]
+        public void FindTHSum_AcceptanceTest()
+        {
+            string input = "89010123\r\n78121874\r\n87430965\r\n96549874\r\n45678903\r\n32019012\r\n01329801\r\n10456732";
+
+            Assert.Equal(36, d10.FindTrailheadSum(input));
+        }
+        [Fact]
+        public void Part1()
+        {
+            string text = File.ReadAllText(@"D:\Projects\AoC\2024\Tests\Input10.txt");
+
+            Assert.Equal(472, d10.FindTrailheadSum(text));
+        }
+        [Fact]
+        public void Part2()
+        {
+            string text = File.ReadAllText(@"D:\Projects\AoC\2024\Tests\Input10.txt");
+
+            Assert.Equal(969, d10.FindTrailheadSumDistinct(text));
+        }
+    }
+}
