e7bc2fe4f3a94b21649d5612e6285a77c29b8dba
Nikola Icic <nikolaicic@gmail.com>
2024-12-22 18:15:52 +0100

    Day 11

---
+++ 2024/Advent2024/Day11.cs
@@ -0,0 +1,29 @@
+﻿using Cache = System.Collections.Concurrent.ConcurrentDictionary<(string, int), long>;
+
+namespace Advent2024
+{
+    public class Day11
+    {
+        public long CountStones(string input, int blinks)
+        {
+            var cache = new Cache();
+            return input.Split(" ").Sum(n => CountStonesHelped(long.Parse(n), blinks, cache));
+        }
+
+        private static long CountStonesHelped(long n, int blinks, Cache cache)
+        {
+            return cache.GetOrAdd((n.ToString(), blinks), key =>
+                key switch {
+                    (_, 0) => 1,
+                    ("0", _) =>
+                        CountStonesHelped(1, blinks - 1, cache),
+                    (var st, _) when st.Length % 2 == 0 =>
+                        CountStonesHelped(long.Parse(st[0..(st.Length / 2)]), blinks - 1, cache) +
+                        CountStonesHelped(long.Parse(st[(st.Length / 2)..]), blinks - 1, cache),
+                    _ =>
+                        CountStonesHelped(2024 * n, blinks - 1, cache)
+                }
+            );
+        }
+    }
+}
---
+++ 2024/Tests/Day11Tests.cs
@@ -0,0 +1,31 @@
+﻿using Advent2024;
+
+namespace Tests
+{
+    public class Day11Tests
+    {
+        Day11 d11 = new Day11();
+
+        [Fact]
+        public void Blinking_AcceptanceTest()
+        {
+            string input = "125 17";
+
+            Assert.Equal(55312, d11.CountStones(input, 25));
+        }
+        [Fact]
+        public void Part1()
+        {
+            string input = "3 386358 86195 85 1267 3752457 0 741";
+
+            Assert.Equal(183248, d11.CountStones(input, 25));
+        }
+        [Fact]
+        public void Part2()
+        {
+            string input = "3 386358 86195 85 1267 3752457 0 741";
+
+            Assert.Equal(0, d11.CountStones(input, 75));
+        }
+    }
+}
